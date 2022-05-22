9d99b767c919f2d4bf11fdaf993330f5d878fd69
limjiewwei <jiewwei@hotmail.com>
2022-05-22 22:02:09 +0800

    updated UI

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,20 +1,10 @@
 import React from "react";
-// .char {
-// 	border-bottom: 3px solid #2980b9;
-// 	display: inline-flex;
-// 	font-size: 30px;
-// 	align-items: center;
-// 	justify-content: center;
-// 	margin: 0 5px;
-// 	height: 50px;
-// 	width: 20px;
-//   }
 const Letters = (props) => {
 	return (
 		<div className="mb-10 text-center">
 			{props.chosenWord.split("").map((char, index) => {
 				return (
-					<span className="border-b-2 border-b-teal-400 w-20 h-10 inline-flex mr-4 justify-center" key={index}>
+					<span className="border-b-2 border-b-teal-400 w-10 h-10 inline-flex mr-4 justify-center" key={index}>
 						{props.correctChar.includes(char) ? char : ""}
 					</span>
 				);
